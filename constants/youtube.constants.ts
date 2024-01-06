86e58ba7bdaeaa386362c21ea52d94a2677eb109
Pisitchai <Tee>
2024-01-07 02:47:14 +0700

    chore:scraperYoutubeLive

---
+++ constants/youtube.constants.ts
@@ -0,0 +1,13 @@
+export const YOUTUBE_LIVE_CHAT_LINK_CONSTANT =
+  "https://www.youtube.com/live_chat?v=";
+
+export const YOUTUBE_HTTP_HEADERS_CONSTANT = {
+  Origin: "https://www.youtube.com",
+  Referer: "https://www.youtube.com",
+  Accept: "*/*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "en-US,en;q=0.8",
+  "Content-Type": "application/json",
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML%2C like Gecko) Chrome/104.0.0.0 Safari/537.36",
+};
---
+++ helpers/scraper.helper.ts
@@ -0,0 +1,46 @@
+import {
+  YOUTUBE_LIVE_CHAT_LINK_CONSTANT,
+  YOUTUBE_HTTP_HEADERS_CONSTANT,
+} from "@/constants/youtube.constants";
+import { YouTubeChatMessageInterface } from "@/interfaces/youtube.interface";
+import puppeteer from "puppeteer";
+
+export const scraperYoutubeLiveChatHelper = async (
+  videoId: string
+): Promise<YouTubeChatMessageInterface[]> => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const url = `${YOUTUBE_LIVE_CHAT_LINK_CONSTANT}${videoId}`;
+
+  await page.setExtraHTTPHeaders({
+    ...YOUTUBE_HTTP_HEADERS_CONSTANT,
+  });
+  await page.goto(url);
+  await page.setViewport({ width: 1080, height: 1024 });
+  await page.waitForSelector("yt-live-chat-text-message-renderer");
+
+  const getComments: YouTubeChatMessageInterface[] = await page.evaluate(() => {
+    const messages: YouTubeChatMessageInterface[] = [];
+    const comments = document.querySelectorAll(
+      "yt-live-chat-text-message-renderer"
+    );
+
+    comments.forEach((comment: Element) => {
+      const authorName = (comment.querySelector("#author-name") as HTMLElement)
+        .innerText;
+      const message = (comment.querySelector("#message") as HTMLElement)
+        .innerText;
+
+      messages.push({
+        authorName,
+        message,
+      });
+    });
+
+    return messages;
+  });
+
+  await browser.close();
+
+  return getComments;
+};
