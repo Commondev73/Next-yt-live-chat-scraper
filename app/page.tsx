4d248c5c409b18124a4520a1d50dec2169caedf1
Pisitchai <Tee>
2024-01-07 20:12:00 +0700

    feat:setup antd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import { scraperYoutubeLiveChatHelper } from "@/helpers/scraper.helper";
 import Image from "next/image";
 
+const yt1 = async () => {
+  const messages = await scraperYoutubeLiveChatHelper("XqTpw4g8ooM");
+  console.log("messages", messages);
+};
+
 export default function Home() {
+  yt1();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
