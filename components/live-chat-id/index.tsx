acf7f5ede3288b80c3266fb31fe7d25773059a4b
Pisitchai <Tee>
2024-01-28 00:49:32 +0700

    feat:use react hook form

--- components/live-chat-id/index.tsx
+++ components/live-chat-id/index.tsx
@@ -5,19 +5,34 @@ import { WechatOutlined } from '@ant-design/icons';
 import { SearchProps } from 'antd/es/input';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 type Props = {
   version: string;
 };
 
+interface Input {
+  liveId: string;
+}
+
+enum InputName {
+  liveId = 'liveId',
+}
+
 const LiveChatId = (props: Props) => {
   const router = useRouter();
+  const { control, formState, handleSubmit, reset } = useForm<Input>();
+  const { errors } = formState;
   const [loading, setLoading] = useState(false);
 
-  const onSearch: SearchProps['onSearch'] = (value, _e, info) => {
+  const onSearch: SearchProps['onSearch'] = () => {
+    handleSubmit(onSubmit)();
+  };
+
+  const onSubmit = (values: Input) => {
     setLoading(true);
-    // console.log(info?.source, value);
-    router.push(`/yt-live-chat/${value}`);
+    reset();
+    router.push(`/yt-live-chat/${values.liveId}`);
   };
 
   return (
@@ -30,14 +45,32 @@ const LiveChatId = (props: Props) => {
           Youtube Live Chat Scraper v{props.version}
         </span>
       </div>
-      <Input.Search
-        placeholder="Live Chat ID"
-        allowClear
-        enterButton
-        size="large"
-        loading={loading}
-        onSearch={onSearch}
+      <Controller
+        control={control}
+        name={InputName.liveId}
+        rules={{
+          required: 'Live ID is required',
+          minLength: 5,
+          maxLength: 100,
+        }}
+        render={({ field }) => (
+          <Input.Search
+            className={`${errors[InputName.liveId] && "rounded-lg border-2 border-red-500"}`}
+            placeholder="Live Chat ID"
+            allowClear
+            enterButton
+            size="large"
+            loading={loading}
+            onSearch={onSearch}
+            {...field}
+          />
+        )}
       />
+      {errors[InputName.liveId] && (
+        <p className="mt-2 text-sm text-red-500">
+          {errors[InputName.liveId].message}
+        </p>
+      )}
     </div>
   );
 };
