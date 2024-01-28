9efa1779c6124282fc89815060434436b9c8a4d0
Pisitchai <Tee>
2024-01-28 23:25:27 +0700

    chore:set delay

--- components/chat-message/index.tsx
+++ components/chat-message/index.tsx
@@ -10,7 +10,7 @@ const ChatMessage = (props: Props) => {
   return (
     <div className='m-2'>
       <Avatar
-        size={{ xs: 24, sm: 32, md: 40, lg: 64 }}
+        size={{ xs: 24, sm: 32, md: 38 }}
         icon={<UserOutlined />}
       />
       <span className="mx-1.5 font-bold">{props.authorName}</span>
